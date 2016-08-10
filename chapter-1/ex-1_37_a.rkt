47a13c7b5903d5132fcb41ef6b6ecf3054f40600
plaguecoder <manish.jaiswal@go-jek.com>
2016-08-10 14:07:45 +0530

    Solution of ex-1.37_a

--- chapter-1/ex-1_37_a.rkt
+++ chapter-1/ex-1_37_a.rkt
@@ -1,15 +1,16 @@
 #lang scheme
 
-(define (cont-frac n d k)
+(define (cont-frac n d k i)
   (if (= k 0)
      0
-     (/ (n 1) (+ (d 1) (cont-frac n d (- k 1))))
+     (/ (n i) (+ (d i) (cont-frac n d (- k 1) (+ i 1))))
   )
   )
 
 (/ 1 (cont-frac (lambda (i) 1.0)
            (lambda (i) 1.0)
-           12))
+           12 1))
+
 
 ;i had to make k as 12 to get an approximation of golden ratio that is
 ;accurate to 4 decimal points.
