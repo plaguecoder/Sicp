c891a2f44fabd3b89a8ad56be9936136a77dfcd6
plaguecoder <manish.jaiswal@go-jek.com>
2016-08-12 13:56:38 +0530

    Solution of ex-2.8

---
+++ Chapter-2/ex-2_8.rkt
@@ -0,0 +1,27 @@
+#lang scheme
+
+(define (make-interval a b) (cons a b)) 
+ (define (upper-bound interval) (max (car interval) (cdr interval))) 
+ (define (lower-bound interval) (min (car interval) (cdr interval))) 
+
+(define (sub-interval x y)                 ; interval subtraction procedure
+  (make-interval (- (lower-bound x) (upper-bound y))
+                 (- (upper-bound x) (lower-bound y))
+                 )
+  )
+
+(define (display-interval i) 
+   (newline) 
+   (display "[") 
+   (display (lower-bound i)) 
+   (display ",") 
+   (display (upper-bound i)) 
+   (display "]"))
+
+(define a (make-interval 4 5))
+(define b (make-interval 6 7))
+
+ (display-interval a)
+ (display-interval b)
+ (display-interval (sub-interval a b)) 
+ (display-interval (sub-interval b a))
