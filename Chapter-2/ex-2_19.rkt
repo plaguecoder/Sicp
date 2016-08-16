8e73d04b151bb42c657263ecafc00abcb323c1f6
plaguecoder <manish.jaiswal@go-jek.com>
2016-08-16 14:28:41 +0530

    Solution of ex-2_19

---
+++ Chapter-2/ex-2_19.rkt
@@ -0,0 +1,36 @@
+#lang scheme
+
+(define us-coins (list 50 25 10 5 1))
+
+(define (first-denomination coin-values)
+  (car coin-values)
+  )
+
+(define (except-first-denomination coin-values)
+  (cdr coin-values)
+  )
+
+(define (no-more? coin-values)
+  (null? coin-values)
+  )
+
+(define (cc amount coin-values)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (no-more? coin-values)) 0)
+        (else
+         (+ (cc amount
+                (except-first-denomination coin-values))
+            (cc (- amount
+                   (first-denomination coin-values))
+                coin-values)
+            )
+         )
+        )
+  )
+
+(cc 100 us-coins)
+
+#|
+order of the list coin values does not matters for the answer as the code takes into
+account all the possible coin combinations.
+|#
