e9d57d3188fba9ffffd62c5a1b3a069c09da3c93
plaguecoder <manish.jaiswal@go-jek.com>
2016-08-23 11:52:33 +0530

    Solution of ex-2_38

---
+++ Chapter-2/ex-2_38.rkt
@@ -0,0 +1,23 @@
+#lang scheme
+
+(define (fold-right op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (fold-right op initial (cdr sequence)))))
+
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
+
+(fold-right / 1 (list 1 2 3))           ;  3/2
+(fold-left / 1 (list 1 2 3))            ;  1/6
+(fold-right list null (list 1 2 3))     ;  (1 (2 (3 ())))
+(fold-left list null (list 1 2 3))      ;  (((() 1) 2) 3)
+
+op has to be associative and commutative both for fold-left and fold-right to produce
+the same results.
